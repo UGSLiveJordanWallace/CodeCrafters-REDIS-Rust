060f1958c40e8d519e3558718edf0962eb3aee4b
UGSLiveJordanWallace <jjbosscore@gmail.com>
2024-08-01 00:41:48 -0400

    stage: respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::net::TcpListener;
+use std::io::{
+    Read, Write
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,8 +11,12 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
