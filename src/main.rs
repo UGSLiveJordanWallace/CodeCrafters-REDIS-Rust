4165f66b54e272ccd6f6e15fa762c92fa7c03bed
UGSLiveJordanWallace <jjbosscore@gmail.com>
2024-08-02 01:05:30 -0400

    stage concurrent connectoins

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
-use std::net::TcpListener;
-use std::io::{
-    Read, Write
-};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    loop {
+        let stream = listener.accept().await;
 
-    for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buf = [0; 512];
-                loop {
-                    let read_buf_count = stream.read(&mut buf).unwrap();
-                    if read_buf_count == 0 {
-                        break;
+            Ok((mut stream, _)) => {
+                tokio::spawn(async move {
+                    let mut buf = [0; 1024];
+                    loop {
+                        let read_buf_count = stream.read(&mut buf).await.unwrap();
+                        if read_buf_count == 0 {
+                            break;
+                        }
+                        stream.write(b"+PONG\r\n").await.unwrap();
                     }
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
+                });
+            },
+            Err (e) => {
+                println!("error: {e}");
             }
         }
     }
